2fe2dfddd885c78572c1ae439421c3743424aa54
Scott Robinson <scott@quadhome.com>
2001-11-28 23:10:01 +0000

    Initial importation of npclient.

---
+++ npclient/vars.h
@@ -0,0 +1,14 @@
+#ifndef __VARS_H__
+#define __VARS_H__
+
+typedef signed char int8;
+typedef signed short int16;
+typedef signed int int32;
+
+typedef unsigned char uint8;
+typedef unsigned short uint16;
+typedef unsigned int uint32;
+
+typedef enum {FALSE, TRUE} bool;
+
+#endif __VARS_H__
