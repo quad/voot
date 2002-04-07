4a8b3096f7dc90d5227b3380b25a64f213a44cf2
Scott Robinson <scott@quadhome.com>
2002-04-07 22:18:23 +0000

    trap logic moved here.

---
+++ exception/2ndstage/old/trap.h
@@ -0,0 +1,21 @@
+#ifndef __TRAP_H__
+#define __TRAP_H__
+
+#include "system.h"
+
+typedef enum
+{
+    NONE,
+    IN,
+    OUT
+} dir_e;
+
+void init_ubc_b_serial(void);
+uint32 trap_inject_data(const uint8 *data, uint32 size);
+void trap_ping_perframe(void);
+void trap_set_passive(bool do_passive);
+void trap_reset(void);
+void* rxi_handler(register_stack *stack, void *current_vector);
+void* serial_handler(register_stack *stack, void *current_vector);
+
+#endif
