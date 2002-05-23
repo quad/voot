606e3c95b032526c026315280ef04b5236dbec1f
cvs2svn <admin@example.com>
2002-05-23 06:06:53 +0000

    This commit was manufactured by cvs2svn to create tag 'cust+'.

--- ubc/descramble.h
+++
@@ -1,7 +0,0 @@
-#ifndef __SCRAMBLE_H__
-#define __SCRAMBLE_H__
-
-void descramble(unsigned char *source, unsigned char *dest, unsigned long size);
-
-#endif
-
--- ubc/ubc-2ndstage/ubc-lowlevel.h
+++
@@ -1,7 +0,0 @@
-#ifndef __UBC_LOWLEVEL_H__
-#define __UBC_LOWLEVEL_H__
-
-void ubc_wait(void);
-void ubc_works();
-
-#endif
