98264ffcce442809feda0309eb1de435c6d6a052
Scott Robinson <scott@quadhome.com>
2002-05-28 16:27:11 +0000

    Initial import of cleaned up np-voot structure.

---
+++ np-voot/loader/graphic/boot.h
@@ -0,0 +1,12 @@
+/*  boot.h
+
+    $Id: boot.h,v 1.1 2002/05/28 16:27:11 quad Exp $
+
+*/
+
+#ifndef __BOOT_H__
+#define __BOOT_H__
+
+void boot_loader (void);
+
+#endif
---
+++ np-voot/loader/graphic/gdrom.h
@@ -0,0 +1,12 @@
+/*  gdrom.h
+
+    $Id: gdrom.h,v 1.1 2002/05/28 16:27:11 quad Exp $
+
+*/
+
+#ifndef __GDROM_H__
+#define __GDROM_H__
+
+void wait_for_gdrom(void);
+
+#endif
