d49f2b0bd2064cb4b100fce1eb5cc4938f38b9e1
cvs2svn <admin@example.com>
2002-06-12 00:41:00 +0000

    This commit was manufactured by cvs2svn to create tag 'init'.

--- exception/2ndstage/old/cdda_patch.h
+++
@@ -1,6 +0,0 @@
-#ifndef __CDDA_PATCH_H__
-#define __CDDA_PATCH_H__
-
-void cdda_enable(void);
-
-#endif
--- exception/2ndstage/scif_emu.h
+++
@@ -1,9 +0,0 @@
-#ifndef __SCIF_EMU_H__
-#define __SCIF_EMU_H__
-
-#include "system.h"
-
-void scif_emu_init(void);
-void* serial_handler(register_stack *stack, void *current_vector);
-
-#endif
