4ef2bd421f9ad4d574f4f3f2e257fd264c82ce27
Scott Robinson <scott@quadhome.com>
2002-06-24 01:09:41 +0000

    Cleaned up the biosfont lowlevel access a bit. Removed the redundant functionality.

--- np-voot/driver/common/biosfont-lowlevel.h
+++ np-voot/driver/common/biosfont-lowlevel.h
@@ -1,16 +1,22 @@
 /*  biosfont-lowlevel.h
 
-    $Id: biosfont-lowlevel.h,v 1.1 2002/06/24 00:58:04 quad Exp $
+    $Id: biosfont-lowlevel.h,v 1.2 2002/06/24 01:09:41 quad Exp $
 
 */
 
 #ifndef __COMMON_BIOSFONT_LOWLEVEL_H__
 #define __COMMON_BIOSFONT_LOWLEVEL_H__
 
+#define BFONT_GET_ADDRESS   0
+#define BFONT_LOCK          1
+#define BFONT_UNLOCK        2
+
 /* NOTE: External definitions. */
 
-extern volatile uint8 * bfont_get_address   (void);
-extern int32            bfont_lock          (void);
-extern void             bfont_unlock        (void);
+extern uint32           bfont_bios          (uint32 call);
+
+#define bfont_get_address() REGISTER(uint8) bfont_bios  (BFONT_GET_ADDRESS)
+#define bfont_lock()        (int32)         bfont_bios  (BFONT_LOCK)
+#define bfont_unlock()      (void)          bfont_bios  (BFONT_UNLOCK)
 
 #endif
--- np-voot/driver/common/biosfont-lowlevel.s
+++ np-voot/driver/common/biosfont-lowlevel.s
@@ -1,29 +1,21 @@
 !   biosfont-lowlevel.s
 !
-!   $Id: biosfont-lowlevel.s,v 1.1 2002/06/24 00:58:04 quad Exp $
+!   $Id: biosfont-lowlevel.s,v 1.2 2002/06/24 01:09:41 quad Exp $
 !
 ! DESCRIPTION
 !
 !   Accessors to the biosfont section of the Dreamcast BIOS.
 !
 
-_bfont_get_address:
-    mov.l   syscall_b4, r0
-    mov.l   @r0, r0
-    jmp     @r0
-    mov     #0, r1
+    .section .text
 
-_bfont_lock:
-    mov.l   syscall_b4, r0
-    mov.l   @r0, r0
-    jmp     @r0
-    mov     #1, r1
+    .global _bfont_bios
 
-_bfont_unlock:
+_bfont_bios:
     mov.l   syscall_b4, r0
     mov.l   @r0, r0
     jmp     @r0
-    mov     #2, r1
+    mov     r4, r1
 
     .align 4
 
